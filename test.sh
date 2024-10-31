c877a23dc46b87477d44069813e4a11d0518ae66
AdUhTkJm <2292398666@qq.com>
2024-10-31 23:25:15 +0000

    Simple RISC-V backend

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm t.s t.exe
+moon run src/bin/main.mbt t.mbt -- -o t.s
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+zig build-exe -target riscv64-linux -femit-bin=t.exe t.s riscv_rt/zig-out/lib/libmincaml.a -O Debug -fno-strip -mcpu=baseline_rv64
+echo "Build finish"
+libriscv/emulator/rvlinux -n t.exe
