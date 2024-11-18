f5b6cd2d42a5bd0746b79d48b57fdc5556f1f09c
AdUhTkJm <2292398666@qq.com>
2024-11-18 14:52:08 +0000

    Fix of spilling double

--- full.py
+++ full.py
@@ -1,28 +1,28 @@
 #!/bin/python3
 import os;
+import random;
 
 files = os.listdir("test/test_src");
+random.shuffle(files);
 
 for x in files:
     name = x[:-4];
-    if x[-4:] == ".ans":
+    if x[-4:] == ".ans" or name == "t":
         continue;
     
-    status = os.system(f"./test.sh {name} > test.txt");
-    if status < 0:
-        print(f"compiler failed on {name}, cannot continue");
-        exit(1);
-    
+    os.system(f"./test.sh {name} > test.txt 2> /dev/null");
+
     result = open("test.txt");
-    content = result.read();
+    content = result.read().strip();
     result.close();
     
     truth = open("test/test_src/" + name + ".ans");
-    content2 = truth.read();
+    content2 = truth.read().strip();
     truth.close();
     
     if content != content2:
         print(f"{name} failed");
+        print(f"expected {content2}, got {content}")
     else:
         print(f"{name} passed");
 
